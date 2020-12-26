ef87d6c4e928181278c21aa56deb925c134a8868
Talekar, Sharad <sharad067@gmail.com>
2020-12-26 15:12:19 +0400

    class function

---
+++ Practise/classfunc.py
@@ -0,0 +1,22 @@
+"""
+Question:
+Define a class which has at least two methods:
+getString: to get a string from console input
+printString: to print the string in upper case.
+Also please include simple test function to test the class methods.
+"""
+
+class InputOutputString(object):
+    def __init__(self):
+        self.s= ""
+
+    def getString(self):
+        self.s= input()
+    
+    def printString(self):
+        print(self.s.upper())
+
+strObj = InputOutputString()
+strObj.getString()
+strObj.printString()
+
